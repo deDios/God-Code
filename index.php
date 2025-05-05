cfd6096727bf9817fc9ecdbdb589aa6f2004620c
Pablo Agustin de Dios Garcia <4855797@gmail.com>
2025-05-05 14:03:13 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobility Solutions Corporation</title>
+    <link rel="shortcut icon" href="/Imagenes/movility.ico" />
+    <link rel="stylesheet" href="CSS/estilos.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+    <!------ Include the above in your HEAD tag ---------->
+</head>
+
+<body>
+<!-- Navigation -->
+<div class="fixed-top">
+  <header class="topbar">
+      <div class="container">
+        <div class="row">
+          <!-- social icon-->
+          <div class="col-sm-12">
+            <ul class="social-network">
+              <li><a class="waves-effect waves-dark" href="https://www.facebook.com/profile.php?id=61563909313215&mibextid=kFxxJD"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="waves-effect waves-dark" href="https://www.instagram.com/mobility__solutions?igsh=MTA5cWFocWhqNmlqYw=="><i class="fa fa-instagram"></i></a></li>
+              <li><a class="waves-effect waves-dark" href="https://mobilitysolutionscorp.com/views/ubicacion.php"><i class="fa fa-map-marker"></i></a></li>
+              <li><a class="waves-effect waves-dark" href="https://mobilitysolutionscorp.com/views/login.php"><i class="fa fa-user"></i></a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+  </header>
+  <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear">
+    <div class="container">
+      <a class="navbar-brand" rel="nofollow" target="_blank" href="https://mobilitysolutionscorp.com"> Mobility Solutions Corporation</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item active">
+            <a class="nav-link" href="https://mobilitysolutionscorp.com">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="https://mobilitysolutionscorp.com/catalogo.php?buscar=&InputMarca=Todos&InputAnio=Todos&InputColor=Todos&InputTransmision=Todos&InputInterior=Todos&InputTipo=Todos&InputPasajeros=Todos&InputMensualidad_Mayor=&InputMensualidad_Menor=&enviar=">Catálogo</a>
+          </li>
+
+         <li class="nav-item">
+            <a class="nav-link" href="https://mobilitysolutionscorp.com/about_us.php">Nosotros</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="https://mobilitysolutionscorp.com/contact.php">Contacto</a>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+</div>
+
+<!-- Carousel -->
+<div class="div_carrusel">
+<div id="demo" class="py-5 carousel slide" data-bs-ride="carousel" py-36>
+
+    <!-- Indicators/dots -->
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
+    </div>
+    <!-- The slideshow/carousel -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <a class="nav-link" href="https://mobilitysolutionscorp.com/catalogo.php?buscar=&InputMarca=Todos&InputAnio=Todos&InputColor=Todos&InputTransmision=Todos&InputInterior=Todos&InputTipo=Todos&InputPasajeros=Todos&InputMensualidad_Mayor=&InputMensualidad_Menor=&enviar=">
+                <img src="Imagenes/Carrusel/carrusel 1.jpg" alt="Los Angeles" class="d-block w-100">
+                </a>
+                </div>
+                <div class="carousel-item">
+                <a class="nav-link" href="https://mobilitysolutionscorp.com/Views/vende.php">
+                  <img src="Imagenes/Carrusel/carrusel 4.jpg" alt="New York" class="d-block w-100">
+                </a>
+                </div>
+                <div class="carousel-item">
+                <img src="Imagenes/Carrusel/carrusel 2.jpg" alt="Chicago" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                <img src="Imagenes/Carrusel/carrusel 3.jpg" alt="New York" class="d-block w-100">
+                </div>
+            </div>
+    <!-- Left and right controls/icons -->
+    <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </button>
+</div>
+</div>
+
+<main>
+  <div class="div_about">
+    <section class="py-3 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="fw-light">🚘 Nuestro balance comercial en números. </h1>
+          <p class="lead text-muted">
+            Accelerate your dreams ✨️
+          </p>
+          <p>
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <div class="album py-3 bg-warning bg-opacity-25">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        
+        <div class="col">
+
+          <div class="count-up-wrapper top">
+            <span id="count-up-container-top">0</span>
+            <span class="additional-info">Entregas 📈</span>
+          </div>
+
+          <script type="module">
+            import { CountUp } from '/js/countup.js/dist/countUP.min.js';
+              const options = {
+                duration: 100,
+                prefix: '+',
+              };
+              let demo = new CountUp('count-up-container-top', 1356, options);
+              if (!demo.error) {
+                demo.start();
+              } else {
+                console.error(demo.error);
+              };
+          </script>
+        </div>
+
+        <div class="col">
+          <div class="count-up-wrapper top">
+            <span id="count-up-container-top2">0</span>
+            <span class="additional-info">MDP Financiado</span>
+          </div>
+
+          <script type="module">
+            import { CountUp } from '/js/countup.js/dist/countUP.min.js';
+              const options2 = {
+                duration: 100,
+                prefix: '+',
+              };
+              let demo2 = new CountUp('count-up-container-top2', 356, options2);
+              if (!demo2.error) {
+                demo2.start();
+              } else {
+                console.error(demo2.error);
+              }
+          </script>
+        </div>
+
+        <div class="col">
+          <div class="count-up-wrapper top">
+            <span id="count-up-container-top3">0</span>
+            <span class="additional-info">Años de experiencia</span>
+          </div>
+          
+          <script type="module">
+            import { CountUp } from '/js/countup.js/dist/countUP.min.js';
+              const options3 = {
+                duration: 100,
+                prefix: '+',
+              };
+              let demo3 = new CountUp('count-up-container-top3', 10, options3);
+              if (!demo3.error) {
+                demo3.start();
+              } else {
+                console.error(demo3.error);
+              }
+          </script>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+  <div class="slider">
+    <div class="slide-track">
+      
+    <?php 
+                $inc = include "db/Conexion.php";
+                    if ($inc){
+                        $query = "select  
+                                    Id, 
+                                    Nombre, 
+                                    Dias, 
+                                    Descripcion
+                                  FROM mobility_solutions.tmx_resenas order by RAND();";
+                        $result = mysqli_query($con,$query);  
+                        if ($result){         
+                            while($row = mysqli_fetch_assoc($result)){
+                                $id = $row['Id'];
+                                $Nombre = $row['Nombre'];
+                                $Dias = $row['Dias'];
+                                $Descripcion = $row['Descripcion'];
+    ?>
+        <div class="cartas pl-3">
+          <div class="card" style="width: 20rem;">
+            <div class="card-body">
+              <div class="titulo_carta">
+                  <div class="Imagenes">
+                    <img src="Imagenes/Perfil_resena/Img<?php echo $id;?>.jpg" alt="Imagen perfil" class="imagen-circular">
+                    <img src="Imagenes/logo_google.jpg" alt="Imagen google" class="imagen-pequeña">
+                  </div>
+                  <div class="Descripcion">
+                      <div class="T_L">
+                        <h5 class="card-title">  <?php echo $Nombre;?> </h5>
+                        <img src="Imagenes/ver.jpg" alt="logo 1">
+                      </div>
+                      <h6 class="card-subtitle mb-2 text-muted"> <?php echo $Dias;?> days ago</h6>
+                  </div>
+              </div>
+              <p class="clasificacion">
+                <label for="radio1">★</label>
+                <label for="radio2">★</label>
+                <label for="radio3">★</label>
+                <label for="radio4">★</label>
+                <label for="radio5">★</label>
+              </p>
+              <p class="card-text"><?php echo $Descripcion;?></p>
+            </div>
+          </div>
+        </div>
+  <?php
+                            }
+                        } else{
+                            echo "Hubo un error en la consulta";
+                        }
+                        mysqli_free_result($result);                  
+                    }
+  ?>
+   
+  
+    </div>
+  </div>
+
+  
+
+</main>
+
+<hr class="mt-5 mb-3"/> 
+
+<footer class="foo mt-5">  
+  <div class="container">    
+    <div class="row">      
+      <div class="col-lg-3">        
+        <h6>Conoce más</h6>  
+        <hr class="hr1 mt-2 mb-3" style="height:5px;border-width:0;color:#FFC00A;background-color:#FFC00A">    
+        <ul class="text-secondary list-unstyled">
+        <li>
+            <a class="text-secondary" href="https://mobilitysolutionscorp.com/about_us.php">¿Quiénes Somos?</a>
+          </li>
+          <li>
+            <a class="text-secondary" href="https://mobilitysolutionscorp.com/Views/vende.php">Vende tu auto</a>
+          </li>
+          <li>
+            <a class="text-secondary" href="https://mobilitysolutionscorp.com/views/ubicacion.php">Sucursales</a>
+          </li>
+          
+        </ul>     
+      </div>      
+      <div class="col-lg-3">        
+        <h6>Legales</h6>    
+        <hr class="hr2 mt-2 mb-3" style="height:5px;border-width:0;color:gainsboro;background-color:gainsboro">     
+        <ul class="text-secondary list-unstyled">
+          <li>
+            <a class="text-secondary" href="/Views/privacy.php">Aviso de privacidad</a>
+          </li>
+        </ul>       
+      </div>      
+      <div class="col-lg-3">        
+        <h6>Ayuda</h6>    
+        <hr class="hr3 mt-2 mb-3" style="height:5px;border-width:0;color:black;background-color:black">    
+        <ul class="text-secondary list-unstyled">
+          <li>
+            <a class="text-secondary" href="https://mobilitysolutionscorp.com/contact.php">Contacto</a>
+          </li>
+          <li>
+            <a class="text-secondary" href="https://mobilitysolutionscorp.com/about_us.php">Preguntas frecuentes</a>
+          </li>
+        </ul>     
+      </div>  
+      <div class="col-lg-3">     
+        <p class="float-end mb-1">
+          <a href="#">Regresa al inicio</a>
+        </p>
+      </div>    
+    </div>  
+  </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
