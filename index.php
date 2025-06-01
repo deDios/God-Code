07a53daef5265d14a61689b0644e050819804435
shannon1052 <shannonkass.2112@gmail.com>
2025-06-01 13:02:37 -0600

    se añadio sección de contenido

--- index.php
+++ index.php
@@ -9,6 +9,22 @@
 </head>
 
 <body>
+    <!-- esta barra en el tope de pagina solo renderiza segun la resolucion de las pantallas -->
+    <div class="social-bar-mobile">
+        <div class="circle-icon-mobile">
+            <img src="ASSETS/index/facebook_icon.png" alt="Facebook" />
+        </div>
+        <div class="circle-icon-mobile">
+            <img src="ASSETS/index/facebook_icon.png" alt="Instagram" />
+        </div>
+        <div class="circle-icon-mobile">
+            <img src="ASSETS/index/facebook_icon.png" alt="TikTok" />
+        </div>
+        <!-- icono de usuario para despues hacer el login pero para la vista mobile-->
+        <div class="user-icon-mobile">
+            <img src="https://img.freepik.com/premium-vector/free-vector-user-icon-simple-line_901408-588.jpg" alt="Usuario" />
+        </div>
+    </div>
 
     <!-- Tope de pagina -->
     <header>
@@ -18,32 +34,78 @@
                 GodCode
             </div>
 
+            <!--Boton hamburguesa que aparece segun la resolucion-->
+            <div class="hamburger" onclick="toggleMenu()">☰</div>
+
             <div class="actions">
-                <button class="btn btn-outline" onclick="location.href='#'">Contáctanos!</button>
                 <button class="btn btn-outline" onclick="location.href='#'">Cotizar</button>
                 <button class="btn btn-primary" onclick="location.href='#'">Registrarse</button>
+                <!-- icono de usuario para despues hacer el login -->
+                <div class="user-icon">
+                    <img src="https://img.freepik.com/premium-vector/free-vector-user-icon-simple-line_901408-588.jpg" alt="Usuario" />
+                </div>
             </div>
+
         </div>
-
-        <!--Boton hamburguesa que aparece segun la resolucion-->
-        <div class="hamburger" onclick="toggleMenu()">☰</div>
-
         <!-- Barra de navegación pequeña -->
         <div id="mobile-menu" class="subnav">
             <a href="#" class="active">Inicio</a>
             <a href="#">Productos</a>
             <a href="#">Quiénes somos</a>
-            <a href="#">Nosotros</a>
+            <a href="#">Ubicación</a>
+
+            <div class="social-icons">
+                <div class="circle-icon">
+                    <img src="ASSETS/index/facebook_icon.png" alt="Facebook" />
+                </div>
+                <div class="circle-icon">
+                    <img src="ASSETS/index/facebook_icon.png" alt="Instagram" />
+                </div>
+                <div class="circle-icon">
+                    <img src="ASSETS/index/facebook_icon.png" alt="TikTok" />
+                </div>
+            </div>
         </div>
     </header>
 
-    <!-- Contenido principal -->
     <main>
-        <h1 class="section-title">Somos...</h1>
-        <p class="section-subtitle">Creamos proyectos web a la medida para tu negocio.</p>
-        <p class="section-subtitle">Contenido.</p>
-    </main>
+        <!-- Contenido principal -->
+        <section class="innovacion-section">
+            <!-- Columna izquierda -->
+            <div class="columna texto">
+                <h1>Desbloquee la innovación,<br> acelere el crecimiento</h1>
+                <p>
+                    IBM LinuxONE Emperor 5 combina Linux e inteligencia artificial para ofrecer seguridad,
+                    rendimiento y eficiencia en la inferencia de IA.
+                </p>
+                <div class="botones">
+                    <a href="#" class="btn btn-primary">Descubra IBM LinuxONE 5</a>
+                    <a href="#" class="btn btn-outline">Explore el conjunto de herramientas de IA</a>
+                </div>
+            </div>
 
+            <!-- Columna central -->
+            <div class="columna imagen">
+                <img src="https://blob.udgtv.com/images/uploads/2017/07/inbox3-focus-0-0-608-342.jpg" />
+            </div>
+
+            <!-- Columna derecha -->
+            <div class="columna noticias">
+                <h3>Noticias</h3>
+                <ul>
+                    <li><a href="#">IBM acelera la revolución de la IA Generativa empresarial con...</a></li>
+                    <li><a href="#">IBM z17: El primer Mainframe totalmente diseñado para la...</a></li>
+                </ul>
+                <div class="paginacion">
+                    <a href="#" class="activo">1</a>
+                    <a href="#">2</a>
+                    <a href="#">3</a>
+                    <a href="#">4</a>
+                </div>
+            </div>
+        </section>
+    </main>
+    
     <!-- Pie de pagina -->
     <footer>
         <div>
