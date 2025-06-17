626f8c7ef1ac545ae6742646002983a4e81c968a
Pablo Agustin de Dios García <pdedios@MacBook-Pro-de-Pablo.local>
2025-06-16 23:54:58 -0600

    servicio web para recuperar cursos v1

---
+++ DB/WEB/consulta_curso.php
@@ -0,0 +1,52 @@
+<?php
+header('Content-Type: application/json');
+
+// Incluir la conexión a la base de datos
+$inc = include "../DB/conn/Conexion.php";
+
+// Consulta a la base de datos
+$query = 'SELECT 
+            id,
+            nombre,
+            descripcion_breve,
+            descripcion_curso,
+            tutor,
+            horas,
+            precio,
+            estatus,
+            creado_por,
+            fecha_creacion,
+            fecha_modif
+          FROM GC_Cursos
+          WHERE estatus = 1
+          ORDER BY nombre ASC;';
+
+$result = mysqli_query($con, $query);
+
+if ($result && $result->num_rows > 0) {
+    $data = array();
+    
+    while ($row = $result->fetch_assoc()) {
+        // Convertir campos numéricos a enteros o float según corresponda
+        $row['id'] = (int)$row['id'];
+        $row['tutor'] = (int)$row['tutor'];
+        $row['horas'] = (float)$row['horas'];
+        $row['precio'] = (int)$row['precio'];
+        $row['estatus'] = (int)$row['estatus'];
+        $row['creado_por'] = (int)$row['creado_por'];
+        
+        // Formatear fechas
+        $row['fecha_creacion'] = $row['fecha_creacion'];
+        $row['fecha_modif'] = $row['fecha_modif'];
+
+        $data[] = $row;
+    }
+
+    echo json_encode($data);
+} else {
+    echo json_encode(["mensaje" => "No se encontraron cursos activos"]);
+}
+
+// Cerrar la conexión
+$con->close();
+?>
---
+++ DB/conn/Conexion.php
@@ -0,0 +1,11 @@
+ <?php
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "/home/site/wwwroot/DB/conn/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($con, "mobilitysolutions-server.mysql.database.azure.com", "btdonyajwn", "Llaverito_4855797'?", "mobility_solutions", 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno()){
+    die('Error: Falla en la conexion de MySQL'. mysqli_connect_errno());
+}
+
+
+?>
