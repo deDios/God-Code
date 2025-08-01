5d366f578bb99249a1fa8b4b122ed2892c5f31bd
shannon1052 <shannonkass.2112@gmail.com>
2025-08-01 17:40:20 -0600

    ajustes visuales en header(mobile) V3

--- VIEW/Login.php
+++ VIEW/Login.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>GodCode</title>
-    <link rel="stylesheet" href="../CSS/Login.css" />
     <link rel="stylesheet" href="../CSS/plantilla.css" />
+    <link rel="stylesheet" href="../CSS/Login.css" />
 </head>
 
 <body>
